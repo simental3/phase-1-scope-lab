c02c565cfd36966c15ad3efe045da74b862429a9
Anthony Simental <anthonysimental12@gmail.com>
2022-12-17 13:22:34 -0800

    commit tree 2f1bafd854f28b390bbcdc67c843d467d4d2f5b7

--- notes.js
+++ notes.js
@@ -1,26 +1,33 @@
-// Write your solution in this file!
 // Examples of what NOT TO DO:
 
+
 // Global Variables
-var customerName = 'bob';   // ** pre-redefined **
-const leastFavoriteCustomer = 'Clippers fan';   // ** pre-redefined **
+var customerName = 'bob';   // => bob   ** pre-redefined **
+const leastFavoriteCustomer = 'Clippers fan';   // => bob   ** pre-redefined **
 
 // Functions
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();      // DO NOT use the return keyword, using it will return 'BOB',
 }                                                   // but it will NOT redefine the variable: customerName
+// upperCaseCustomerName();
+// console.log('redefined', customerName);  // => BOB
 
 
 function setBestCustomer() {
     bestCustomer = 'not bob';
 }
+// setBestCustomer();
+// console.log('bestCustomer', bestCustomer);  // => not bob
 
 
 function overwriteBestCustomer() {
     bestCustomer = 'maybe bob';
 }
+// overwriteBestCustomer();
+// console.log('reassigned', bestCustomer);  // => maybe bob
 
 
 function changeLeastFavoriteCustomer() {
     leastFavoriteCustomer = 'Warriors fan';
 }
+// console.log('changeLeastFavoriteCustomer', changeLeastFavoriteCustomer());
